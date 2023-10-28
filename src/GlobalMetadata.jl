f271d8673ca729ae37c769a777ef504cdc1fb92e
Zachary P. Christensen <zchristensen7@gmail.com>
2023-10-28 00:43:37 -0400

    Large refactor using reflection to accomodate DataAPI metadata methods

--- src/GlobalMetadata.jl
+++ src/GlobalMetadata.jl
@@ -1,146 +1,118 @@
 module GlobalMetadata
 
-export @metadata, @metadata!
+using DataAPI
 
-const GLOBAL_METADATA    = gensym(:metadata)
+const META = gensym(:metadata)
 
-struct NoData end
-const no_data = NoData()
-haskey(::NoData, _) = false
+# this shouldn't be constructed directly
+# versions below 1.10 don't support the module in the type space.
+# * < v"1.10" `Static` is `nothing` or `() -> mod::Module`
+# * >= v"1.10" `Static` is `nothing` or `mod::Module`
+struct ThisModule{
+    STATIC,
+    D<:Union{Nothing, Module},
+    support
+}
+    dynamic::D
 
-# retrieve metadata in module `m` for object `obj` associated with key `k`. if `k` is not not found `data` is assigned to the key.
-global_object_metadata!(m::Module, x, k, data) = get!(global_object_metadata!(m, x), k, data)
-#endregion
-
-function global_metadata(m::Module)
-    isdefined(m, GLOBAL_METADATA) && return getfield(m, GLOBAL_METADATA)
-    error("Module $(m) does not have any global metadata.")
+    global function _ThisModule(s, d, support::NamedTuple{(:read, :write), Tuple{Bool, Bool}})
+        new{s, typeof(d), support}(d)
+    end
 end
-global_metadata(m::Module, x) = global_metadata(m)[objectid(x)]
-global_metadata(m::Module, x, key) = global_metadata(m)[objectid(x)][key]
-
-"""
-    @metadata(x[, k])
-
-Retreive metadata associated with the object id of `x` (`objectid(x)`) in the current
-module's global metadata. If the key `k` is specified only the value associated with
-that key is returned.
-"""
-macro metadata()
-    esc(:(GlobalMetadata.global_metadata(@__MODULE__)))
-end
-macro metadata(x)
-    esc(:(GlobalMetadata.global_metadata(@__MODULE__, $(x))))
-end
-macro metadata(x, key)
-    esc(:(GlobalMetadata.global_metadata(@__MODULE__, $(x), $(key))))
+@static if VERSION < v"1.10"
+    (tm::ThisModule{STATIC})() where {STATIC} = isa(STATIC, Function) ? STATIC() : getfield(tm, 1)
+    _this_module_expr(m::Module, nt) = :(const This = $(_ThisModule)(() -> $(m), nothing, $(nt)))
+else
+    (tm::ThisModule{STATIC})() where {STATIC} = isa(STATIC, Module) ? STATIC : getfield(tm, 1)
+    _this_module_expr(m::Module, nt) = :(const This = $(_ThisModule)($(m), nothing, $(nt)))
 end
 
-# has_global_metadata
-has_global_metadata(m::Module) = isdefined(m, GLOBAL_METADATA)
-function has_global_metadata(m::Module, x)
-    has_global_metadata(m) && haskey(getfield(m, GLOBAL_METADATA), objectid(x))
+@noinline function throw_read_error(m::Module)
+    throw(ArgumentError("Module $(m)'s metadata does not support read access."))
 end
-function has_global_metadata(m::Module, x, k)
-    has_global_metadata(m) && haskey(get(getfield(m, GLOBAL_METADATA), objectid(x), no_data), k)
+@noinline function throw_write_error(m::Module)
+    throw(ArgumentError("Module $(m)'s metadata does not support write access."))
 end
 
-"""
-    @has_metadata(x)::Bool
-    @has_metadata(x, k)::Bool
-
-Does `x` have metadata stored in the curren modules' global metadata? Checks for the
-presenece of the key `k` if specified.
-"""
-
-macro has_metadata(x)
-    esc(:(GlobalMetadata.has_global_metadata(@__MODULE__, x)))
+DataAPI.metadatasupport(::Type{<:ThisModule{<:Any, <:Any, support}}) where {support} = support
+function DataAPI.metadata(tm::ThisModule, key::AbstractString; style::Bool=false)
+    DataAPI.metadata(tm, Symbol(key); style=style)
 end
-macro has_metadata(x, k)
-    esc(:(GlobalMetadata.has_global_metadata(@__MODULE__, x, k)))
-end
-
-#=
-    @has_metadata()::Bool
-macro has_metadata()
-    esc(:(GlobalMetadata.has_global_metadata(@__MODULE__)))
-end
-=#
-
-# global_metadata!
-function init_global_metadata!(m::Module, data=no_data)
-    if has_global_metadata(m)
-        return getfield(m, GLOBAL_METADATA)
+@inline function DataAPI.metadata(tm::ThisModule, key::Symbol; style::Bool=false)
+    mod = tm()
+    DataAPI.metadatasupport(typeof(tm)).read || throw_read_error(mod)
+    if style
+        return (getfield(mod, META)[key], :default)
     else
-        gm = data === no_data ? IdDict{UInt,Dict{Symbol,Any}}() : data
-        Core.eval(m, :(const $GLOBAL_METADATA = $(gm)))
-        return gm
+        return getfield(mod, META)[key]
     end
 end
-function global_metadata!(m::Module, x, data=no_data)
-    gm = init_global_metadata!(m)
-    id = objectid(x)
-    xm = get(gm, id, no_data)
-    if xm === no_data
-        xm = data === no_data ? Dict{Symbol,Any}() : data
-        gm[id] = xm
-        if _attach_global_metadata!(x, gm, id)
-            @warn "Cannot create finalizer for $(typeof(x)). Global dictionary must be manually deleted."
-        end
-        return xm
+function DataAPI.metadata(tm::ThisModule, key::AbstractString, default; style::Bool=false)
+    DataAPI.metadata(tm, Symbol(key), default; style=style)
+end
+@inline function DataAPI.metadata(tm::ThisModule, key::Symbol, default; style::Bool=false)
+    mod = tm()
+    DataAPI.metadatasupport(typeof(tm)).read || throw_read_error(mod)
+    if style
+        return (get(mod, key, default), :default)
     else
-        return xm
+        return get(mod, key, default)
     end
 end
-function _attach_global_metadata!(x, gm, id)
-    there_is_no_finalizer = true
-    if ismutable(x)
-        _assign_global_metadata_finalizer(x,gm, id)
-        there_is_no_finalizer = false
-    elseif isstructtype(typeof(x))
-        N = fieldcount(typeof(x))
-        if N !== 0
-            i = 1
-            while (there_is_no_finalizer && i <= N)
-                there_is_no_finalizer = _assign_global_metadata_finalizer(getfield(x, i), gm, id)
-                i += 1
-            end
-        end
+DataAPI.metadatakeys(tm::ThisModule) = keys(getfield(tm(), META))
+
+function DataAPI.metadata!(tm::ThisModule, key::AbstractString, value; style::Symbol=:default)
+    DataAPI.metadata!(tm, Symbol(key), value)
+end
+function DataAPI.metadata!(tm::ThisModule, key::Symbol, value; style::Symbol=:default)
+    mod = tm()
+    DataAPI.metadatasupport(typeof(tm)).write || throw_write_error(mod)
+    setindex!(getfield(mod, META), value, key)
+end
+
+function DataAPI.deletemetadata!(tm::ThisModule, key::AbstractString)
+    DataAPI.deletemetadata!(tm::ThisModule, Symbol(key))
+end
+function DataAPI.deletemetadata!(tm::ThisModule, key::Symbol)
+    mod = tm()
+    DataAPI.metadatasupport(typeof(tm)).write || throw_write_error(mod)
+    delete!(getfield(mod, META), key)
+end
+function DataAPI.emptymetadata!(tm::ThisModule)
+    mod = tm()
+    DataAPI.metadatasupport(typeof(tm)).write || throw_write_error(mod)
+    empty!(getfield(mod, META))
+end
+
+@nospecialize
+function initmeta(
+    m::Module,
+    md::AbstractDict{Symbol, Any};
+    read::Bool = false,
+    write::Bool = false,
+)
+    if !isdefined(m, META)
+        Core.eval(m, Expr(:block, _this_module_expr(m, (; read, write)), :(const $(META) = $(md))))
     end
-    return there_is_no_finalizer
+    nothing
 end
-function _assign_global_metadata_finalizer(x, gm, id)
-    if Base.ismutable(x)
-        finalizer(x) do _
-            @async delete!(gm, id)
-        end
-        return false
-    else
-        return true
+Base.show(io::IO, tm::ThisModule) = show(io, MIME"text/plain"(), tm)
+function Base.show(io::IO, m::MIME"text/plain", tm::ThisModule)
+    if isa(x, typeof(this))
+        return print(io, "this")
+    elseif isa(x, typeof(This))
+        return print(io, "This")
+    elseif isa(x, ThisModule)
+        show(io, tm()::Module)
+        print(io, " --> this")
     end
-end
-global_metadata!(m::Module, x, k, val) = get!(global_metadata(m, x), k, val)
-
-"""
-    @metadata!(x[data = Dict{Symbol,Any}()])
-
-Initializes global metadata for `x` to `data`. If `data` is not provided then the default is
-`Dict{Symbol,Any}()`.
-"""
-macro metadata!(x)
-    esc(:(GlobalMetadata.global_metadata!(@__MODULE__, $(x))))
-end
-macro metadata!(x, data)
-    esc(:(GlobalMetadata.global_metadata!(@__MODULE__, $(x), $(data))))
+    nothing
 end
 
-"""
-    @metadata!(x, k, val)
+@specialize
+# GlobalAlias
+# GlobalValue
 
-Set the value of `x`'s global metadata associated with the key `k` to `val`.
-"""
-macro metadata!(x, k, val)
-    esc(:(GlobalMetadata.global_metadata!(@__MODULE__, $(x), $(k), $(val))))
-end
+initmeta(@__MODULE__, IdDict{Symbol, Any}(); read=true, write=true)
 
 end
